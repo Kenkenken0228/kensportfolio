ae226d83b015059ec4f034c6f6d042517635b81c
Ken567502 <123934467+Ken567502@users.noreply.github.com>
2023-06-22 16:18:01 +0800

    INSANE EPIC WEBSITE

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter } from "react-router-dom"
+
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+
+const App = () => {
+
+  return (
+    <BrowserRouter>
+    <div className="relative z-0 bg-more">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <StarsCanvas />
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      
+      <Experience />
+      <Tech />
+      <Works />
+      <div className="relative z-0">
+        <Contact />
+    
+      </div>
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
