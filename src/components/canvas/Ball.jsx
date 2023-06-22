ada41c241eed839586eb5627cc9086def66045ff
Kenkenken0228 <137218260+Kenkenken0228@users.noreply.github.com>
2023-06-22 17:06:42 +0800

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,50 +1,18 @@
-import React, { Suspense }from 'react'
-import { Canvas } from '@react-three/fiber'
-import {
-  Decal, Float, OrbitControls, Preload, useTexture,
-} from '@react-three/drei'
+import React from 'react'
+import Tilt from "react-tilt";
 
-import CanvasLoader from '../Loader'
-
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl])
-
+const BallCanvas = ({icon,name}) => {
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25}/>
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronBufferGeometry args={[1, 1]} />
-        <meshStandardMaterial 
-          color="#6B799E"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal 
-          position={[0, 0, 1]}
-          rotation={[ 2 * Math.PI, 0, 6.25]}
-          flatShading
-          map={decal}
-        />
-      </mesh>
-    </Float>
-  )
-}
-
-const BallCanvas = ({ icon }) => {
-  return (
-    <Canvas
-    frameloop='demand'
-    gl={{ preserveDrawingBuffer: true }}
+    <Tilt
+    options={{
+      max: 45,
+      scale: 1,
+      speed: 150,
+    }}
+    className='w-[100px] h-[100px] rounded-full bg-[#333333bb] p-4 ball_card'
   >
-    <Suspense fallback={<CanvasLoader />}>
-      <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon}/>
-    </Suspense>
-
-    <Preload all />
-  </Canvas>
+      <img src={icon} alt="tech Icons" title={name} className="w-full h-full object-cover " />
+    </Tilt>
   )
 }
 
